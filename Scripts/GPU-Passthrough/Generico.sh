38f6ebddde9251ff00d296c7f361e3d34749764a
Daniel <danilop95puentegar@gmail.com>
2023-07-08 23:31:07 +0200

    1

--- Scripts/GPU-Passthrough/Generico.sh
+++ Scripts/GPU-Passthrough/Generico.sh
@@ -1,117 +1,72 @@
 #!/bin/bash
 
-# Función para buscar el dispositivo GPU
-search_gpu_device() {
-    echo "Por favor, introduce el nombre del dispositivo que estás buscando:"
-    read dispositivo
-    lspci -v | grep -i "$dispositivo"
+# Función para agregar una entrada a un archivo si no está presente
+add_to_file_if_not_present() {
+    local filename="$1"
+    local entry="$2"
+    if ! grep -Fxq "$entry" "$filename"; then
+        echo "$entry" | sudo tee -a "$filename"
+    fi
+}
+
+# Función para leer el tipo de CPU
+read_cpu_type() {
+    echo "¿Qué tipo de CPU tienes? (Intel/AMD):"
+    read CPU_TYPE
+    CPU_TYPE=$(echo "$CPU_TYPE" | tr '[:upper:]' '[:lower:]')
 }
 
 # Función para leer el ID del GPU
 read_gpu_id() {
+    echo "Por favor, introduce el nombre del dispositivo que estás buscando:"
+    read DEVICE_NAME
+    echo "Estos son los dispositivos que coinciden con su búsqueda:"
+    lspci -v | grep -i "$DEVICE_NAME"
     echo "Ingrese la ID del dispositivo de video (formato xx:xx.x):"
     read GPU_ID
-    echo "Obteniendo la ID de su GPU:"
-    GPU_VENDOR_ID=$(lspci -n -s $GPU_ID | awk '{print $3}')
-    echo $GPU_VENDOR_ID
 }
 
-# Función para agregar una entrada a la lista negra si no está presente
-add_to_blacklist() {
-    local entry="blacklist $1"
-    if ! grep -Fxq "$entry" /etc/modprobe.d/blacklist.conf; then
-        echo "$entry" | sudo tee -a /etc/modprobe.d/blacklist.conf
-    fi
-}
+# Llamada a las funciones para leer los datos del usuario
+read_cpu_type
+read_gpu_id
 
-# Función para verificar si IOMMU está habilitado
-verify_iommu() {
-    echo "Verificando si IOMMU está habilitado..."
-    dmesg | grep -e DMAR -e IOMMU
-}
-
-# Función para verificar si se admite el remapeo de interrupciones
-verify_interrupt_remap() {
-    echo "Verificando si el remapeo de interrupciones está habilitado..."
-    dmesg | grep 'remapping'
-}
-
-# Función para verificar el aislamiento de IOMMU
-verify_iommu_isolation() {
-    echo "Verificando el aislamiento de IOMMU..."
-    pvesh get /nodes/{nodename}/hardware/pci --pci-class-blacklist ""
-}
-
-# Función para agregar opciones de MSI a la configuración de audio
-add_msi_options() {
-    echo "Agregando opciones de MSI para dispositivos de audio..."
-    echo "options snd-hda-intel enable_msi=1" | sudo tee -a /etc/modprobe.d/snd-hda-intel.conf
-}
-
-# Función para verificar si el dispositivo GPU es compatible con UEFI
-verify_uefi_compatibility() {
-    echo "Verificando si el dispositivo de GPU es compatible con UEFI..."
-    # Agrega aquí tu código de verificación de compatibilidad con UEFI
-}
-
-# Función para preguntar si se quiere reiniciar
-ask_reboot() {
-    read -p "¿Quieres reiniciar ahora? (s/n) " RESPUESTA
-    case $RESPUESTA in
-        [sS])
-            echo "Reiniciando el sistema..."
-            sudo reboot
-            ;;
-        *)
-            echo "Por favor, recuerda reiniciar el sistema manualmente."
-            ;;
-    esac
-}
-
-# Script principal
-
-# 1. Selección de CPU
-echo "Seleccione el tipo de su CPU (1 para Intel, 2 para AMD):"
-select CPU_TYPE in Intel AMD
-do
-    case $CPU_TYPE in
-        Intel)
-            sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on"/' /etc/default/grub
-            break
-            ;;
-        AMD)
-            sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet amd_iommu=on"/' /etc/default/grub
-            break
-            ;;
-        *)
-            echo "Por favor, seleccione 1 o 2."
-            ;;
-    esac
-done
+# Configuración de Grub
+if [[ $CPU_TYPE == "intel" ]]
+then
+    sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on"/' /etc/default/grub
+else
+    sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet amd_iommu=on"/' /etc/default/grub
+fi
 sudo update-grub
 
-echo 'vfio
-vfio_iommu_type1
-vfio_pci
-vfio_virqfd' | sudo tee -a /etc/modules
+# Módulos VFIO
+add_to_file_if_not_present "/etc/modules" "vfio"
+add_to_file_if_not_present "/etc/modules" "vfio_iommu_type1"
+add_to_file_if_not_present "/etc/modules" "vfio_pci"
+add_to_file_if_not_present "/etc/modules" "vfio_virqfd"
 
+# Interrupción IOMMU
 echo "options vfio_iommu_type1 allow_unsafe_interrupts=1" | sudo tee -a /etc/modprobe.d/iommu_unsafe_interrupts.conf
 echo "options kvm ignore_msrs=1" | sudo tee -a /etc/modprobe.d/kvm.conf
 
-add_to_blacklist "radeon"
-add_to_blacklist "nouveau"
-add_to_blacklist "nvidia"
+# Lista negra de controladores
+add_to_file_if_not_present "/etc/modprobe.d/blacklist.conf" "blacklist radeon"
+add_to_file_if_not_present "/etc/modprobe.d/blacklist.conf" "blacklist nouveau"
+add_to_file_if_not_present "/etc/modprobe.d/blacklist.conf" "blacklist nvidia"
 
-search_gpu_device
-read_gpu_id
-echo "options vfio-pci ids=$GPU_VENDOR_ID disable_vga=1" | sudo tee -a /etc/modprobe.d/vfio.conf
+# Obtención de las ID del proveedor
+GPU_VENDOR_ID=$(lspci -n -s "$GPU_ID" | awk '{print $3}')
 
+# Adición del ID del proveedor a VFIO
+echo "options vfio-pci ids=$GPU_VENDOR_ID disable_vga=1" | sudo tee /etc/modprobe.d/vfio.conf
+
+# Actualización de initramfs
 sudo update-initramfs -u
 
-verify_iommu
-verify_interrupt_remap
-verify_iommu_isolation
-add_msi_options
-verify_uefi_compatibility
-
-ask_reboot
+# Opción de reinicio
+echo "¿Quieres reiniciar ahora? (s/n)"
+read REBOOT
+REBOOT=$(echo "$REBOOT" | tr '[:upper:]' '[:lower:]')
+if [[ $REBOOT == "s" ]]; then
+    sudo reboot
+fi
