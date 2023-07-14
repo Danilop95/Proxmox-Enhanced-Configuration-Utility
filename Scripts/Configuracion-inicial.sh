e5e31a8146e93fb62c4b10b60c97f49f1f7ba142
DVNILXP <59531081+Danilop95@users.noreply.github.com>
2023-07-14 14:58:45 +0200

    Fix sources.list
    /etc/apt/sources.list
     
    Borrar /etc/apt/sources.list.d

--- Scripts/Configuracion-inicial.sh
+++ Scripts/Configuracion-inicial.sh
@@ -1,15 +1,45 @@
 #!/bin/bash
 
-# Verificar e instalar sudo si no está presente
-if ! command -v sudo &> /dev/null
-then
-    echo "sudo no está instalado. Intentando instalar sudo..."
+# Comprobar si el usuario es root
+if [[ $EUID -ne 0 ]]; then
+    echo "Este script debe ser ejecutado como root."
+    exit 1
+fi
+
+# Comprobar si sudo está instalado, si no, instalarlo.
+if ! command -v sudo &> /dev/null; then
+    echo "sudo no está instalado. Instalando..."
     apt-get update
     apt-get install sudo -y
 fi
 
 # Función para preguntar si se desea continuar
 ask_continue() {
+    echo "El siguiente paso consistirá en:"
+    case $1 in
+        "backup")
+            echo "Hacer una copia de seguridad de tu archivo sources.list."
+            ;;
+        "modify")
+            echo "Modificar el archivo sources.list."
+            ;;
+        "update")
+            echo "Actualizar el sistema."
+            ;;
+        "install")
+            echo "Instalar herramientas necesarias."
+            ;;
+        "clone")
+            echo "Clonar repositorios necesarios."
+            ;;
+        "install_rust")
+            echo "Instalar Rust."
+            ;;
+        "install_pve")
+            echo "Descargar e instalar los encabezados de Proxmox VE."
+            ;;
+    esac
+
     read -p "¿Deseas continuar con el siguiente paso? (y/n) " answer
     case $answer in
         [yY])
@@ -22,14 +52,11 @@ ask_continue() {
     esac
 }
 
-# Nombre del archivo a modificar
-filename="/etc/apt/sources.list"
-
 # Función para crear una copia de seguridad del archivo
 backup_file() {
     echo "Creando una copia de seguridad de tu archivo sources.list..."
     backup_filename="/etc/apt/sources.list.bak_$(date +%Y%m%d_%H%M%S)"
-    sudo cp "$filename" "$backup_filename"
+    sudo cp "/etc/apt/sources.list" "$backup_filename"
     echo "Copia de seguridad creada en $backup_filename."
 }
 
@@ -38,54 +65,53 @@ modify_sources_list() {
     echo "Modificando el archivo sources.list..."
     
     # Borrar el contenido actual del archivo
-    sudo echo "" > "$filename"
+    sudo echo "" > "/etc/apt/sources.list"
 
     # Añadir las entradas al archivo
-    echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" | sudo tee -a "$filename"
-    echo "deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription" | sudo tee -a "$filename"
+    echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" | sudo tee -a "/etc/apt/sources.list"
+    echo "deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription" | sudo tee -a "/etc/apt/sources.list"
 
-    # Eliminar el archivo de repositorio Enterprise si existe
-    if [ -f /etc/apt/sources.list.d/pve-enterprise.list ]; then
-        sudo rm /etc/apt/sources.list.d/pve-enterprise.list
+    # Eliminar el directorio de repositorio Enterprise si existe
+    if [ -d /etc/apt/sources.list.d/ ]; then
+        sudo rm -rf /etc/apt/sources.list.d/
     fi
 
     echo "Archivo sources.list modificado."
 }
 
 # Ejecutar las funciones
-ask_continue && backup_file
-ask_continue && modify_sources_list
+ask_continue "backup" && backup_file
+ask_continue "modify" && modify_sources_list
 
 # Continuar con la instalación de dependencias
 echo 'Instalación de dependencias...'
-ask_continue || exit
+ask_continue "update" || exit
 
 # Actualizar el sistema
 echo "Actualizando el sistema..."
 sudo apt update && sudo apt -y upgrade
-ask_continue || exit
+ask_continue "install" || exit
 
 # Instalar herramientas necesarias
 echo "Instalando herramientas necesarias..."
 sudo apt -y install git build-essential pve-headers dkms jq mdevctl
-ask_continue || exit
+ask_continue "clone" || exit
 
 # Clonar repositorios necesarios
 echo "Clonando repositorios necesarios..."
 git clone https://github.com/DualCoder/vgpu_unlock
 git clone https://github.com/mbilker/vgpu_unlock-rs
-ask_continue || exit
+ask_continue "install_rust" || exit
 
 # Instalar Rust
 echo "Instalando Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-ask_continue || exit
+ask_continue "install_pve" || exit
 
 # Descargar e instalar los encabezados de Proxmox VE
-echo "Descargando e instalando los encabezados de Proxmox VE..."
+echo "Descargando e instalar los encabezados de Proxmox VE..."
 wget http://download.proxmox.com/debian/dists/bullseye/pve-no-subscription/binary-amd64/pve-headers-5.15.30-2-pve_5.15.30-3_amd64.deb
 sudo dpkg -i pve-headers-5.15.30-2-pve_5.15.30-3_amd64.deb
-ask_continue || exit
 
 # Informar al usuario sobre la necesidad de descargar manualmente el controlador de nVidia vGPU
 echo "Descargue el controlador v14.0 nVidia vGPU para Linux KVM desde https://nvid.nvidia.com"
