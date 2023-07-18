df94fe7d27a5bd409142dfb90f87ce54409e35f7
Daniel <danilop95puentegar@gmail.com>
2023-07-18 18:49:21 +0200

    fix

--- Configurador.sh
+++ Configurador.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Colors
-ROJO='\e[0;91m'
-VERDE='\e[0;92m'
-AZUL='\e[0;94m'
+ROJO='\e[0;31m'
+VERDE='\e[0;32m'
+AZUL='\e[0;34m'
 NC='\e[0m' # No Color
 
 # Function to display language selection menu
 menu_idioma() {
     clear
     echo -e "${AZUL}=================================================${NC}"
-    echo -e "${AZUL}|      Seleccione el Idioma/Select Language      |${NC}"
+    echo -e "${AZUL}|              Seleccione el Idioma              |${NC}"
     echo -e "${AZUL}=================================================${NC}"
-    echo -e "${VERDE} 1) Español${NC}"
-    echo -e "${VERDE} 2) English${NC}"
+    echo -e "${AZUL} 1) Español${NC}"
+    echo -e "${AZUL} 2) English${NC}"
     echo -e "${AZUL}=================================================${NC}"
     read -p "$(echo -e ${AZUL}Seleccione una opción:${NC} )" opcion
 
@@ -21,12 +21,12 @@ menu_idioma() {
         1)
             echo -e "${AZUL}Seleccionado: Español${NC}"
             sleep 2
-            script_espanol
+            ejecutar_script "es.sh"
             ;;
         2)
             echo -e "${AZUL}Selected: English${NC}"
             sleep 2
-            script_english
+            ejecutar_script "en.sh"
             ;;
         *)
             echo -e "${ROJO}Opción inválida.${NC}"
@@ -36,16 +36,15 @@ menu_idioma() {
     esac
 }
 
-# Function for Spanish script
-script_espanol() {
-    # Coloca aquí el contenido del script en español
-    echo -e "${VERDE}¡Has seleccionado el script en español!${NC}"
-}
-
-# Function for English script
-script_english() {
-    # Put the content of the English script here
-    echo -e "${VERDE}You have selected the script in English!${NC}"
+# Function to execute the selected script
+ejecutar_script() {
+    local script="$1"
+    if [ -f "$script" ]; then
+        chmod +x "$script"
+        ./"$script"
+    else
+        echo -e "${ROJO}El archivo del script no existe.${NC}"
+    fi
 }
 
 # Run the language selection menu
