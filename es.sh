166a0e84739109f1497f865a95acee47c6cfe9a2
DVNILXP <59531081+Danilop95@users.noreply.github.com>
2023-07-18 19:32:32 +0200

    Update es.sh

--- es.sh
+++ es.sh
@@ -1,157 +1,318 @@
 #!/bin/bash
 
-# Colors
-RED='\e[0;31m'
-GREEN='\e[0;32m'
-BLUE='\e[0;34m'
-YELLOW='\e[0;33m'
-NC='\e[0m' # No Color
+# Colores
+ROJO='\e[0;31m'
+VERDE='\e[0;32m'
+AZUL='\e[0;34m'
+AMARILLO='\e[0;33m'
+NC='\e[0m' # Sin color
 
-# Backup directory
-BACKUP_DIRECTORY="$(dirname "$0")/backup-script"
+# Ruta del directorio de backup
+BACKUP_DIR="$(dirname "$0")/backup-script"
 
-# Check if the user is root
+# Comprobar si el usuario es root
 if [[ $EUID -ne 0 ]]; then
-    echo -e "${RED}This script must be run as root.${NC}"
+    echo -e "${ROJO}Este script debe ser ejecutado como root.${NC}"
     exit 1
 fi
 
-# Check if sudo is installed, if not, install it.
+# Comprobar si sudo está instalado, si no, instalarlo.
 if ! command -v sudo &> /dev/null; then
-    echo -e "${BLUE}sudo is not installed. Installing...${NC}"
+    echo -e "${AZUL}sudo no está instalado. Instalando...${NC}"
     apt-get update
     apt-get install sudo -y
 fi
 
-# Function to ask if the user wants to continue
-ask_to_continue() {
-    echo -e "${BLUE}=================================================${NC}"
-    echo -e "${BLUE}| The following step will:                       |${NC}"
-    echo -e "${BLUE}=================================================${NC}"
-    echo -e "${GREEN}$1${NC}"
-    echo -e "${BLUE}-------------------------------------------------${NC}"
-    echo -n -e "${BLUE}Do you want to continue?${NC} (y/n): "
-    read response
-    case $response in
-        [yY])
-            echo -e "${BLUE}Continuing...${NC}"
+# Función para preguntar si se desea continuar
+preguntar_continuar() {
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${AZUL}| El siguiente paso consistirá en:             |${NC}"
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${VERDE}$1${NC}"
+    echo -e "${AZUL}-------------------------------------------------${NC}"
+    echo -n -e "${AZUL}¿Deseas continuar?${NC} (s/n): "
+    read respuesta
+    case $respuesta in
+        [sS])
+            echo -e "${AZUL}Continuando...${NC}"
             ;;
         *)
-            echo -e "${RED}Skipping to the next step.${NC}"
+            echo -e "${ROJO}Saltando al siguiente paso.${NC}"
             return 1
             ;;
     esac
 }
 
-# Function to create a backup of the file
-create_backup() {
-    echo -e "${BLUE}=================================================${NC}"
-    echo -e "${BLUE}| Creating a backup of sources.list              |${NC}"
-    echo -e "${BLUE}=================================================${NC}"
+# Función para crear una copia de seguridad del archivo
+crear_copia_seguridad() {
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${AZUL}| Creando una copia de seguridad de sources.list |${NC}"
+    echo -e "${AZUL}=================================================${NC}"
 
-    # Check if the backup directory exists
-    if [[ ! -d "$BACKUP_DIRECTORY" ]]; then
-        mkdir -p "$BACKUP_DIRECTORY"
-        echo -e "${GREEN}Backup directory created at $BACKUP_DIRECTORY.${NC}"
+    # Verificar si el directorio de backup existe
+    if [[ ! -d "$BACKUP_DIR" ]]; then
+        mkdir -p "$BACKUP_DIR"
+        echo -e "${VERDE}Directorio de backup creado en $BACKUP_DIR.${NC}"
     fi
 
-    # Limit the maximum number of backups to 5
-    backup_files=("${BACKUP_DIRECTORY}/sources.list.bak_"*)
-    if [[ ${#backup_files[@]} -ge 5 ]]; then
-        echo -e "${BLUE}Maximum number of backups reached. Delete some backups to create new ones.${NC}"
+    # Limitar el número máximo de backups a 5
+    backups=("${BACKUP_DIR}/sources.list.bak_"*)
+    if [[ ${#backups[@]} -ge 5 ]]; then
+        echo -e "${AZUL}El número máximo de backups ha sido alcanzado. Elimina algunos backups para hacer nuevos.${NC}"
         return 1
     fi
 
-    backup_file="${BACKUP_DIRECTORY}/sources.list.bak_$(date +%Y%m%d_%H%M%S)"
-    sudo cp "/etc/apt/sources.list" "$backup_file"
-    echo -e "${GREEN}Backup created at $backup_file.${NC}"
+    archivo_backup="${BACKUP_DIR}/sources.list.bak_$(date +%Y%m%d_%H%M%S)"
+    sudo cp "/etc/apt/sources.list" "$archivo_backup"
+    echo -e "${VERDE}Copia de seguridad creada en $archivo_backup.${NC}"
 }
 
-# Function to restore a previous backup of the file
-restore_backup() {
-    echo -e "${BLUE}=================================================${NC}"
-    echo -e "${BLUE}| Restoring a previous backup                    |${NC}"
-    echo -e "${BLUE}=================================================${NC}"
-    backup_files=("${BACKUP_DIRECTORY}/sources.list.bak_"*)
+# Función para restaurar una copia anterior del archivo
+restaurar_copia_seguridad() {
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${AZUL}| Restaurando una copia anterior de sources.list |${NC}"
+    echo -e "${AZUL}=================================================${NC}"
+    backups=("${BACKUP_DIR}/sources.list.bak_"*)
 
-    if [[ ${#backup_files[@]} -eq 0 ]]; then
-        echo -e "${RED}No backups available.${NC}"
+    if [[ ${#backups[@]} -eq 0 ]]; then
+        echo -e "${ROJO}No hay copias de seguridad disponibles.${NC}"
         return 1
     fi
 
-    echo -e "${BLUE}Select a backup to restore:${NC}"
-    for ((i=0; i<${#backup_files[@]}; i++)); do
-        echo -e "${BLUE} $((i+1)))${NC} ${backup_files[$i]}"
+    echo -e "${AZUL}Selecciona una copia de seguridad para restaurar:${NC}"
+    for ((i=0; i<${#backups[@]}; i++)); do
+        echo -e "${AZUL} $((i+1)))${NC} ${backups[$i]}"
     done
 
-    read -p "$(echo -e ${BLUE}Enter the backup number:${NC} )" backup_number
+    read -p "$(echo -e ${AZUL}Ingresa el número de la copia de seguridad:${NC} )" numero_copia
 
-    # Check if the backup number is valid
-    if ! [[ "$backup_number" =~ ^[1-5]$ ]]; then
-        echo -e "${RED}Invalid option.${NC}"
+    # Verificar si el número de copia de seguridad es válido
+    if ! [[ "$numero_copia" =~ ^[1-5]$ ]]; then
+        echo -e "${ROJO}Opción inválida.${NC}"
         return 1
     fi
 
-    backup_file="${backup_files[$((backup_number-1))]}"
-    if [[ -f "$backup_file" ]]; then
-        echo -e "${BLUE}Preview of the backup file:${NC}"
+    archivo_copia="${backups[$((numero_copia-1))]}"
+    if [[ -f "$archivo_copia" ]]; then
+        echo -e "${AZUL}Vista previa del archivo de backup:${NC}"
         echo "-----------------------------------------"
-        cat "$backup_file"
+        cat "$archivo_copia"
         echo "-----------------------------------------"
-        echo -n -e "${BLUE}Do you want to restore this backup?${NC} (y/n): "
-        read response
-        case $response in
-            [yY])
-                sudo cp "$backup_file" "/etc/apt/sources.list"
-                echo -e "${GREEN}Backup restored successfully.${NC}"
+        echo -n -e "${AZUL}¿Deseas restaurar esta copia de seguridad?${NC} (s/n): "
+        read respuesta
+        case $respuesta in
+            [sS])
+                sudo cp "$archivo_copia" "/etc/apt/sources.list"
+                echo -e "${VERDE}Copia de seguridad restaurada.${NC}"
                 ;;
             *)
-                echo -e "${RED}Operation canceled.${NC}"
+                echo -e "${ROJO}Operación cancelada.${NC}"
                 ;;
         esac
     else
-        echo -e "${RED}Backup file not found.${NC}"
-        return 1
+        echo -e "${ROJO}El archivo de backup no existe.${NC}"
     fi
 }
 
-# Function to edit the sources.list file
-edit_sources_list() {
-    echo -e "${BLUE}=================================================${NC}"
-    echo -e "${BLUE}|          Editing sources.list file             |${NC}"
-    echo -e "${BLUE}=================================================${NC}"
-    echo -e "${YELLOW}The sources.list file will be opened in the text editor. Make the necessary changes.${NC}"
-    echo -e "${YELLOW}When you are done, save the changes and close the text editor to continue.${NC}"
-    echo -e "${YELLOW}Press Enter to continue.${NC}"
-    read
-
-    sudo $EDITOR /etc/apt/sources.list
-    echo -e "${GREEN}sources.list file edited successfully.${NC}"
+# Función para abrir el archivo sources.list con nano
+abrir_sources_list() {
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${AZUL}| Abriendo el archivo sources.list con nano      |${NC}"
+    echo -e "${AZUL}=================================================${NC}"
+    sudo nano "/etc/apt/sources.list"
 }
 
-# Main function to execute the configuration steps
-execute_configuration() {
-    echo -e "${BLUE}=================================================${NC}"
-    echo -e "${BLUE}|          Proxmox VE Configuration             |${NC}"
-    echo -e "${BLUE}=================================================${NC}"
-
-    ask_to_continue "Create a backup of sources.list"
-    if [[ $? -eq 0 ]]; then
-        create_backup
-    fi
-
-    ask_to_continue "Restore a previous backup of sources.list"
-    if [[ $? -eq 0 ]]; then
-        restore_backup
-    fi
-
-    ask_to_continue "Edit the sources.list file"
-    if [[ $? -eq 0 ]]; then
-        edit_sources_list
-    fi
-
-    echo -e "${GREEN}Configuration completed.${NC}"
+# Función para verificar si una línea existe en el archivo sources.list
+linea_existe() {
+    local linea="$1"
+    grep -Fxq "$linea" "/etc/apt/sources.list"
 }
 
-# Execute the main function
-execute_configuration
+# Función para modificar el archivo sources.list
+modificar_sources_list() {
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${AZUL}|          Modificando el archivo sources.list         |${NC}"
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${AMARILLO}Agrega las líneas necesarias al archivo sources.list.${NC}"
+
+    # Verificar si las líneas ya existen antes de agregarlas
+    if linea_existe "deb http://ftp.debian.org/debian bullseye main contrib" && \
+       linea_existe "deb http://ftp.debian.org/debian bullseye-updates main contrib" && \
+       linea_existe "deb http://security.debian.org/debian-security bullseye-security main contrib" && \
+       linea_existe "# PVE pve-no-subscription repository provided by proxmox.com, NOT recommended for production use" && \
+       linea_existe "deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription"
+    then
+        echo -e "${VERDE}El archivo sources.list ya contiene las modificaciones.${NC}"
+    else
+        # Agregar repositorios de Debian
+        echo "deb http://ftp.debian.org/debian bullseye main contrib" | sudo tee -a "/etc/apt/sources.list"
+        echo "deb http://ftp.debian.org/debian bullseye-updates main contrib" | sudo tee -a "/etc/apt/sources.list"
+        echo "deb http://security.debian.org/debian-security bullseye-security main contrib" | sudo tee -a "/etc/apt/sources.list"
+
+        # Agregar repositorio de Proxmox VE
+        echo "# PVE pve-no-subscription repository provided by proxmox.com, NOT recommended for production use" | sudo tee -a "/etc/apt/sources.list"
+        echo "deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription" | sudo tee -a "/etc/apt/sources.list"
+
+        echo -e "${VERDE}Archivo sources.list modificado.${NC}"
+    fi
+}
+
+# Función para agregar opciones de MSI al archivo de configuración de audio
+agregar_opciones_msi() {
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${AZUL}| Agregando opciones de MSI para audio           |${NC}"
+    echo -e "${AZUL}=================================================${NC}"
+    agregar_a_archivo_si_no_existe "/etc/modprobe.d/snd-hda-intel.conf" "options snd-hda-intel enable_msi=1"
+    echo -e "${VERDE}Opciones de MSI agregadas.${NC}"
+}
+
+# Función para verificar si IOMMU está habilitado
+verificar_iommu() {
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${AZUL}| Verificando si IOMMU está habilitado           |${NC}"
+    echo -e "${AZUL}=================================================${NC}"
+    dmesg | grep -e DMAR -e IOMMU
+}
+
+# Función para aplicar la configuración del kernel
+aplicar_configuracion_kernel() {
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${AZUL}| Aplicando configuración del kernel             |${NC}"
+    echo -e "${AZUL}=================================================${NC}"
+    sudo update-initramfs -u -k all
+    echo -e "${VERDE}Configuración del kernel aplicada.${NC}"
+}
+
+# Función para preguntar si se quiere reiniciar
+preguntar_reinicio() {
+    echo -e "${AZUL}=================================================${NC}"
+    echo -n -e "${AZUL}¿Quieres reiniciar ahora?${NC} (s/n): "
+    read respuesta
+    case $respuesta in
+        [sS])
+            echo -e "${AZUL}Reiniciando el sistema...${NC}"
+            sudo reboot
+            ;;
+        *)
+            echo -e "${AZUL}Por favor, recuerda reiniciar el sistema manualmente.${NC}"
+            ;;
+    esac
+}
+
+# Función para agregar una entrada a un archivo si no está presente
+agregar_a_archivo_si_no_existe() {
+    local archivo="$1"
+    local entrada="$2"
+    if ! grep -Fxq "$entrada" "$archivo"; then
+        echo "$entrada" | sudo tee -a "$archivo"
+    fi
+}
+
+# Función para buscar el dispositivo GPU
+buscar_dispositivo_gpu() {
+    echo -e "${AZUL}Por favor, introduce el nombre del dispositivo que estás buscando (ejemplo: GTX 1080):${NC}"
+    read dispositivo
+    lspci -v | grep -i "$dispositivo"
+}
+
+# Función para leer el ID del GPU
+leer_id_gpu() {
+    echo -e "${AZUL}Ingrese la ID del dispositivo de video (formato xx:xx.x):${NC}"
+    read GPU_ID
+    echo -e "${AZUL}Obteniendo la ID de su GPU:${NC}"
+    GPU_VENDOR_ID=$(lspci -n -s "$GPU_ID" | awk '{print $3}')
+    echo $GPU_VENDOR_ID
+}
+
+# Función para configurar el passthrough de GPU
+configurar_passthrough_gpu() {
+    echo -e "${AZUL}=================================================${NC}"
+    echo -e "${AZUL}|        Configuración GPU Passthrough           |${NC}"
+    echo -e "${AZUL}=================================================${NC}"
+
+    # Buscar GPU y leer su ID
+    buscar_dispositivo_gpu
+    leer_id_gpu
+
+    # Añadir controladores a la lista negra
+    agregar_a_archivo_si_no_existe "/etc/modprobe.d/blacklist.conf" "blacklist nouveau"
+    agregar_a_archivo_si_no_existe "/etc/modprobe.d/blacklist.conf" "blacklist nvidia"
+
+    # Agregar opciones de VFIO e IOMMU
+    agregar_a_archivo_si_no_existe "/etc/modprobe.d/vfio.conf" "options vfio-pci ids=$GPU_VENDOR_ID disable_vga=1"
+
+    aplicar_configuracion_kernel
+
+    preguntar_reinicio
+}
+
+# Función principal del script
+principal() {
+    while true; do
+        clear
+        echo -e "${AZUL}=================================================${NC}"
+        echo -e "${AZUL}|           Menú de Opciones                    |${NC}"
+        echo -e "${AZUL}=================================================${NC}"
+        echo -e "${AZUL} 1) Instalación de dependencias${NC}"
+        echo -e "${AZUL} 2) Configuración GPU Passthrough${NC}"
+        echo -e "${AZUL} 3) Salir${NC}"
+        echo -e "${AZUL}=================================================${NC}"
+        read -p "$(echo -e ${AZUL}Selecciona una opción:${NC} )" opcion
+
+        case $opcion in
+            1)
+                while true; do
+                    clear
+                    echo -e "${AZUL}=================================================${NC}"
+                    echo -e "${AZUL}|    Menú de Opciones (Instalación dependencias) |${NC}"
+                    echo -e "${AZUL}=================================================${NC}"
+                    echo -e "${AZUL} 1) Hacer una copia de seguridad de sources.list${NC}"
+                    echo -e "${AZUL} 2) Restaurar una copia anterior de sources.list${NC}"
+                    echo -e "${AZUL} 3) Modificar archivo sources.list${NC}"
+                    echo -e "${AZUL} 4) Abrir sources.list con nano${NC}"
+                    echo -e "${AZUL} 5) Volver al menú principal${NC}"
+                    echo -e "${AZUL}=================================================${NC}"
+                    read -p "$(echo -e ${AZUL}Selecciona una opción:${NC} )" opcion_deps
+
+                    case $opcion_deps in
+                        1)
+                            crear_copia_seguridad
+                            sleep 2
+                            ;;
+                        2)
+                            restaurar_copia_seguridad
+                            sleep 2
+                            ;;
+                        3)
+                            modificar_sources_list
+                            sleep 2
+                            ;;
+                        4)
+                            abrir_sources_list
+                            sleep 2
+                            ;;
+                        5)
+                            break
+                            ;;
+                        *)
+                            echo -e "${ROJO}Opción inválida.${NC}"
+                            sleep 2
+                            ;;
+                    esac
+                done
+                ;;
+            2)
+                configurar_passthrough_gpu
+                ;;
+            3)
+                exit
+                ;;
+            *)
+                echo -e "${ROJO}Opción inválida.${NC}"
+                ;;
+        esac
+    done
+}
+
+# Ejecutar el script
+principal
